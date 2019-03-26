7754acbf382b11073fe8a3f35c9548e7391205c1
melisaaltinsoy <32497953+melisaaltinsoy@users.noreply.github.com>
2019-03-26 11:25:57 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var song;
+
+function preload(){
+     song = loadSound("rainbow.mp3");
+}
+
+function setup(){
+    createCanvas(200,200);
+    song.play();
+}
+
+function draw(){
+    background(0);
+}
